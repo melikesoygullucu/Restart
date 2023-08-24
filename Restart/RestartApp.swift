8d85dc4be7eb3e00290d5100abc5ee3f48869a90
melikesoygullucu <melikesoygul@hotmail.com>
2023-08-24 14:15:10 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Melike on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
